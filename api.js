e72f6c8d624b71bd50268a96cc68c1a259920587
douwevdijk <douwe@into-output.com>
2014-03-04 10:13:34 +0100

    Change URL

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
-var server = new Server('{URL}', 33669, {auto_reconnect: true});
+var server = new Server({URL}, 33669, {auto_reconnect: true});
 db = new Db('placius', server, {safe: true});
    
 app.listen(3000);
